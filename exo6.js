2c695e98cf4dc020da81a5a0061428bb9963a888
c-bel <c.hris@laposte.net>
2016-11-07 15:25:11 +0100

    first commit

---
+++ exo6.js
@@ -0,0 +1,7 @@
+function division(){
+  var premier = document.getElementById('premier_nombre').value;
+  var deuxieme = document.getElementById('deuxieme_nombre').value;
+
+  var resultat = premier % deuxieme;
+  alert("Le résultat de la division de " + premier + " par " + deuxieme + " est : " + resultat);
+}
